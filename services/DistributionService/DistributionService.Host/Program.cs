41ac17b400caaca3029e28c66d38b7f1fe28953b
Косторной Дмитрий Вадимович <D1.imosa@yandex.ru>
2026-03-29 05:02:00 +0500

    feat(distribution-service): add Host layer

--- services/DistributionService/DistributionService.Host/Program.cs
+++ services/DistributionService/DistributionService.Host/Program.cs
@@ -1,7 +1,56 @@
-using DistributionService.Host;
+using DistributionService.Application;
+using DistributionService.Infrastructure;
+using DistributionService.Infrastructure.Data;
+using SharedHosting;
+using SharedHosting.Extensions;
+using SharedHosting.Options;
+using AvroSchemas;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = WebApplication.CreateBuilder(args);
 
-var host = builder.Build();
-host.Run();
+builder.Services.AddSharedHosting(builder.Configuration, new HostingOptions
+    {
+        ServiceName = "DistributionService",
+        Environment = builder.Environment.EnvironmentName,
+        EnableSwagger = builder.Environment.IsDevelopment(),
+        EnableHealthChecks = true
+    }, additionalTelemetrySources:
+    [
+        "DistributionService.DistributionProcessor"
+    ]);
+
+builder.Services.Configure<DatabaseOptions>(
+    builder.Configuration.GetSection(DatabaseOptions.SectionName));
+builder.Services.Configure<KafkaOptions>(
+    builder.Configuration.GetSection(KafkaOptions.SectionName));
+
+builder.Services.AddApplication();
+
+builder.Services.AddSharedDbContext<ApplicationDbContext>(builder.Configuration);
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSharedHosting();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.ApplyMigrationsAsync<ApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    await DbInitializer.SeedAsync(context, logger);
+
+    await KafkaExtensions.WaitForKafkaTopicsAsync(app.Services, [
+        "lead-events",
+        "distribution-events",
+        "notification-events"
+    ]);
+
+    var schemaRegistry = app.Services.GetRequiredService<Confluent.SchemaRegistry.ISchemaRegistryClient>();
+    await SchemaRegistryHelper.RegisterAllSchemasAsync(schemaRegistry, logger);
+}
+
+await app.RunAsync();
